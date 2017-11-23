1e047aab1bfa3e391a3c15385f5d345ace24080f
Smilyan Dichev <dahaca6022@gmail.com>
2017-11-23 20:30:30 +0200

    i updated the input conversion and made the vars easier to read

--- Hex2Dec.js
+++ Hex2Dec.js
@@ -1,22 +1,28 @@
 function Hex2Dec (input) {
-    function HexFinder(input) {
+    function HexFinder(number) {
         
-        switch (input) {
+        switch (number) {
             case 'A': return 10; 
             case 'B': return 11; 
             case 'C': return 12; 
             case 'D': return 13; 
             case 'E': return 14; 
             case 'F': return 15;
-            default: return +input;
+            default: return +number;
         }
     }
     let result = 0;
-    for (let i = input.length - 1; i >= 0; i -= 1) {
-        result += HexFinder(input[i]) * Math.pow(16,input.length - 1 - i);
+    let hexStr = input.toString();
+    let foundHex;
+    let power;
+    for (let i = hexStr.length - 1; i >= 0; i -= 1) {
+        foundHex = HexFinder(hexStr[i]);
+        power = Math.pow(16,hexStr.length - 1 - i);
+        result += foundHex * power;
     }
     console.log(result);
 }
 Hex2Dec('1AE3'); //6883
 Hex2Dec('4ED528CBB4');
 Hex2Dec('FE'); //338583669684
+//
